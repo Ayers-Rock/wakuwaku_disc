11aec360d6fe781d41fca423932b7a321f553527
高橋智也 <takahashitomoya@takahashitomoyanoMacBook-Pro.local>
2019-10-10 19:32:02 +0900

    createアクション作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,5 +18,8 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 end
