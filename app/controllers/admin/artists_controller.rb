a61bd0322302b56ffb22fd8f9332130a88719dee
Spanaic <spanaic@gmail.com>
2019-10-12 15:33:11 +0900

    機能追加完了

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,10 +1,23 @@
 class Admin::ArtistsController < ApplicationController
   def new
+    @artist = Artist.new
+    @artists = Artist.all
   end
 
   def create
+    artist = Artist.new(artist_params)
+    artist.save
+    redirect_to new_admin_artist_path
   end
 
   def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+    redirect_to new_admin_artist_path
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:artist_name)
   end
 end
--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,24 @@
 class Admin::GenresController < ApplicationController
   def new
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to new_admin_genre_path
+    # TODO:バリデーションに伴うエラー表示やフラッシュメッセージが必要な場合は条件分岐などを追記
   end
 
   def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to new_admin_genre_path
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:genre_name)
   end
 end
