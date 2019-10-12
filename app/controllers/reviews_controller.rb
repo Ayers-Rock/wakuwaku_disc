bdf332921cc85e32a5e710518e2fbdf1081a337a
Spanaic <spanaic@gmail.com>
2019-10-12 19:24:39 +0900

    仮で機能を実装中

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
   def create
+    review = Review.new(review_params)
+    review.item_id = Item.find(params[:id])
+    current_user = review.user_id
+    review.save
+    redirect_to item_path(params[:id])
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:title, :body)
   end
 end
