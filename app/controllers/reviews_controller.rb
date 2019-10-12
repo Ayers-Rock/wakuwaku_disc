edc91a5875244e13f990314ef635d907070b73b3
Spanaic <spanaic@gmail.com>
2019-10-12 19:59:27 +0900

    微修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def create
     review = Review.new(review_params)
-    review.item_id = Item.find(params[:id])
+    review.item_id = Item.find(params[:item_id])
     current_user = review.user_id
     review.save
     redirect_to item_path(params[:id])
