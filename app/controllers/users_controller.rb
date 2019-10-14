6a5776714456a74fce57b62af093b5f2fb9095b5
trudemui <alesth_130@yahoo.co.jp>
2019-10-14 12:33:50 +0900

    壊れた復旧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "you have updated user successfully."
+    end
   end
 end
