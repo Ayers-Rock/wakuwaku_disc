2b4aea450ba3bace6e38f243fbd4db0cea22c772
Spanaic <spanaic@gmail.com>
2019-10-15 18:41:28 +0900

    deviseエラー未解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_103303) do
+ActiveRecord::Schema.define(version: 2019_10_12_084247) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -24,18 +24,6 @@ ActiveRecord::Schema.define(version: 2019_10_13_103303) do
     t.string "building"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "artists", force: :cascade do |t|
