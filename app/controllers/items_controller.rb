622391b5b3514e805f32089ef33fa0b11dac9719
trudemui <alesth_130@yahoo.co.jp>
2019-10-15 14:15:27 +0900

    途中

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
 
   def ranking
     @items = Item.all
+    @all_ranks = Item.find(favorite.group(:note_id).order('count(item_id)desc').limit(20).plack(item_id)
   end
 
   def show
