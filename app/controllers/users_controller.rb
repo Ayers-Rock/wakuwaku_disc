0ad3a0125492ce01ba9c34444547aed9cfe71265
nozomi_h <hakamata.m14@gmail.com>
2019-10-16 15:58:00 +0900

    cart_item作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @favolites = Favolite.limit(5).favolite('id')
+    #@favolites = Favolite.limit(5).favolite('id')
     @orders = Order.limit(3).order('id')
   end
 
