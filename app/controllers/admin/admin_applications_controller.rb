0fbfeb231e6658b8221ece38a15f161b3b3cd059
nozomi_h <hakamata.m14@gmail.com>
2019-10-16 11:47:27 +0900

    devise 90%

--- app/controllers/admin/admin_applications_controller.rb
+++ app/controllers/admin/admin_applications_controller.rb
@@ -2,13 +2,13 @@ class Admin::AdminApplicationsController < ApplicationController
     layout "admin_application"
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-	def after_sign_in_path_for(resource)
-		admin_path(resource)
-	end
+	# def after_sign_in_path_for(resource)
+	# 	admin_path(resource)
+	# end
 
-	def after_sign_out_path_for(resource)
-		new_admin_session_path
-	end
+	# def after_sign_out_path_for(resource)
+	# 	new_admin_session_path
+	# end
 
 	# protected
 	# def configure_permitted_parameters
