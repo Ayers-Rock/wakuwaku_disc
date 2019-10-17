90585d49609d81d92756da09152b5f020501caea
nozomi_h <hakamata.m14@gmail.com>
2019-10-17 23:53:23 +0900

    index修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,13 @@
 class Admin::OrdersController < Admin::AdminApplicationsController
   def index
+    @orders = Order.all
   end
 
   def show
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item)
   end
 end
--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,6 +12,8 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def index
