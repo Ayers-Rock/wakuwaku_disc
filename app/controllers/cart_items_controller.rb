810294d33caf3392cf77cac45ca124ce158b05ec
nozomi_h <hakamata.m14@gmail.com>
2019-10-17 15:42:55 +0900

    cart_itemほぼ完

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class CartItemsController < ApplicationController
   def create
-    @cart_item = CartItem.new(user_id)
+    @cart_item = CartItem.new(cart_item_params)
     @cart_item.user_id = current_user.id
     if @cart_item.save
     redirect_to cart_items_path
@@ -16,5 +16,10 @@ class CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.where(user_id: current_user.id)
     # s@cart_items = current_user.cart_items
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
