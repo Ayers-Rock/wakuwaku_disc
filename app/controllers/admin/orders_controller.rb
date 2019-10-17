76352aead04966361d1f020cd344401700ae450b
nozomi_h <hakamata.m14@gmail.com>
2019-10-17 12:16:55 +0900

    ページャー取り込み途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,9 @@
-class Admin::OrdersController < Admin::AdminApplicationsController
+class Admin::OrdersController < Admin::AdminApplicationsControll
+  
+  PER = 8
+
   def index
-    @order_items = Order.all
+    @orders = Order.page(params[:page]).per(PER)
   end
 
   def show
