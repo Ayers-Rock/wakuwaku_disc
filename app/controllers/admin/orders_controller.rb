fcc19f4f67130d0818ce5bc178eb0fc5dd6226b8
nozomi_h <hakamata.m14@gmail.com>
2019-10-19 01:26:36 +0900

    order index/show 修正途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,9 @@
 class Admin::OrdersController < Admin::AdminApplicationsController
   
-  PER = 10
+  PER = 9
 
   def index
+    @order = Order.all
     @orders = Order.page(params[:page]).per(PER)
   end
 
