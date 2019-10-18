a5afc88b4015575523a0dce26d8655b3aafefd06
Spanaic <spanaic@gmail.com>
2019-10-18 13:36:07 +0900

    showページでのいいね非同期は確認おｋ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,12 @@ class FavoritesController < ApplicationController
   def create
     item = Item.find(params[:item_id])
     favorite = current_user.favorites.new(item_id: item.id)
+    # binding.pry
     favorite.save
 
     @items=Item.all
-    @item = favorite.item
+    # @item = favorite.
+    @item = item
     @favorites = Favorite.where(user_id: current_user.id)
     @cart_item = CartItem.new
   end
