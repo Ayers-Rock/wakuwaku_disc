6f92caef06189ef9f3f168810167042bab816bac
trudemui <alesth_130@yahoo.co.jp>
2019-10-19 11:21:55 +0900

    形だけ

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,10 @@
 class Admin::UsersController < Admin::AdminApplicationsController
   def index
+    @users = User.all
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
@@ -10,12 +12,24 @@ class Admin::UsersController < Admin::AdminApplicationsController
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_path
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update
+      redirect_to admin_user_path(@user.id)
+    end
   end
 
   def orders
   end
+
+  private
+def user_params
+    params.require(:user).permit(:first_name, :last_name, :first_nana_name, :last_kana_name, :tel_number, :prefecture, :city_address, :building, :postal_code, :email, :password)
+end
 end
 
