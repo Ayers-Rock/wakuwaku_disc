118d6ca2363fe1864361308285b8112e30ad2bc1
Spanaic <spanaic@gmail.com>
2019-10-19 13:02:02 +0900

    ブランチを変更します

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,8 @@ class Stock < ApplicationRecord
 
     validates :arrival_count, presence: true
     validates :arrival_date, presence: true
+
+    def stock_sum(item)
+        stock_total = Stock.where(item_id: item.id).sum(:arrival_count)
+    end
 end
