44db00f331cc1441d75dff6a0400187f0e1c7359
nozomi_h <hakamata.m14@gmail.com>
2019-10-19 13:58:58 +0900

    cart_item/destroy完

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,16 +5,20 @@ class CartItemsController < ApplicationController
     if @cart_item.save
     redirect_to cart_items_path
     else
-    #@cart_item = 
     render 'items/index'
     end
   end
 
   def destroy
+    @item = CartItem.find(params[:id])
+      @item.destroy
+      redirect_to cart_items_path
   end
 
   def index
     @cart_items = CartItem.where(user_id: current_user.id)
-    # s@cart_items = current_user.cart_items
+    if @cart_items.empty?
+      redirect_to root_path
+    end
   end
 end
