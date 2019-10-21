d4657d8ac37ccb6329d85b08d949fa80c2fb7341
Spanaic <spanaic@gmail.com>
2019-10-21 18:28:41 +0900

    個数変更機能（カート内）追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,16 @@
 class ItemsController < ApplicationController
   def rankings
-  
+
   end
 
   def ranking
     @items = Item.all
     # @all_ranks = Item.find(favorite.group(:item_id).order('count(item_id)desc').limit(20).plack(item_id)
+
+      # @monthly_ranks = Item.find(OrderItem.where(item_id: item.id).sum(:amount).group(:item_id).order(amount: "desc")).where(created_at: 1.month.ago.beginning_of_day..Time.zone.now.end_of_day)
+    # binding.pry
+    @monthly_ranks = OrderItem.where(created_at: 1.days.ago.beginning_of_day..Time.zone.now.end_of_day).group(:item_id).sum(:amount).sort_by{ | k, v | v }.reverse
+      # @monthly_ranks = OrderItem.where(created_at: 1.days.ago.beginning_of_day..Time.zone.now.end_of_day).group(:item_id).order('sum(:amount) desc')
   end
 
   def show
