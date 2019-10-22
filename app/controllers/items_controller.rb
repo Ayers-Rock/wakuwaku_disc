01830219055715bd3001b3bc008a24b1697846c6
高橋智也 <takahashitomoya@takahashitomoyanoMacBook-Pro.local>
2019-10-22 20:49:55 +0900

    一旦コミットします

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,11 @@
 class ItemsController < ApplicationController
   def rankings
-    @items = Item.all
-    @favorite_ranks = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(3).pluck(:item_id))
-    @monthly_ranks = Item.where(created_at: 1.months.ago.beginning_of_day..Time.zone.now.end_of_day).find(OrderItem.group(:item_id).order('sum(amount) desc').limit(3).pluck(:item_id))
-    @weekly_ranks = Item.where(created_at: 1.weeks.ago.beginning_of_day..Time.zone.now.end_of_day).find(OrderItem.group(:item_id).order('sum(amount) desc').limit(5).pluck(:item_id))
-    @new_cd =Item.limit(6).order(" created_at DESC ")
+  
   end
 
   def ranking
-    @favorite_ranks = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(20).pluck(:item_id))
-    @monthly_ranks = Item.where(created_at: 1.months.ago.beginning_of_day..Time.zone.now.end_of_day).find(OrderItem.group(:item_id).order('sum(amount) desc').limit(20).pluck(:item_id))
-    @weekly_ranks = Item.where(created_at: 1.weeks.ago.beginning_of_day..Time.zone.now.end_of_day).find(OrderItem.group(:item_id).order('sum(amount) desc').limit(20).pluck(:item_id))
-    @cart_item = CartItem.new
+    @items = Item.all
+    # @all_ranks = Item.find(favorite.group(:item_id).order('count(item_id)desc').limit(20).plack(item_id)
   end
 
   def show
@@ -19,19 +13,15 @@ class ItemsController < ApplicationController
     # @user = User.find(params[:id])
     @cart_item = CartItem.new
     @reviews = Review.where(item_id: @item.id)
+    # binding.pry
     @review = Review.new
   end
 
   def index
-    unless params[:search].blank?
-      @artist = Item.joins(:artist).where("artist_name LIKE ?", "%#{params[:search]}%")
-      @track =  Item.joins(discs: :tracks).where("track_name LIKE ?", "%#{params[:search]}%")
-      @title = Item.where("title LIKE ?", "%#{params[:search]}%")
-      @merged_result = @artist | @title
-      @items = @merged_result | @track
-    else
-      @items = Item.all
-    end
+    @items = Item.all
     @cart_item = CartItem.new
   end
+
+
 end
+    
