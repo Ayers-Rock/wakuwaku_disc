deb843a3dce432b26eac41431ed109b30fce7196
nozomi_h <hakamata.m14@gmail.com>
2019-10-24 18:50:19 +0900

    pull t

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,7 +5,7 @@ class CartItemsController < ApplicationController
     @cart_item.amount = params[:cart_item][:amount].to_i
     @cart_item.item_id = params[:cart_item][:item_id].to_i
     if @cart_item.save
-    redirect_to cart_items_path
+      redirect_to user_cart_items_path(current_user.id)
     else
       @items = Item.page(params[:page]).per(10).reverse_order
       render 'items/index'
@@ -15,13 +15,37 @@ class CartItemsController < ApplicationController
   def destroy
     @item = CartItem.find(params[:id])
       @item.destroy
-      redirect_to cart_items_path
+      redirect_to user_cart_items_path(current_user.id)
   end
 
-  def index
+  def update
+    @cart_items = CartItem.where(user_id: current_user.id)
+    @cart_items.each_with_index do |cart_item, i|
+
+    if cart_item.update(amount: params[:amount][i.to_s])
+    else
+      @items = Item.page(params[:page]).per(10).reverse_order
+      render 'items/index'
+    end
+      # binding.pry
+      # @cart_item.amount = params[:cart_item][:amount].to_i
+      # @cart_item.item_id = params[:cart_item][:item_id].to_i
+
+    # if cart_item.update(amount: params[:amount][i.to_s])
+    #   redirect_to new_order_path
+    # else
+    #   @items = Item.page(params[:page]).per(10).reverse_order
+    #   render 'items/index'
+    # end
+    redirect_to new_order_path
+  end
+
+  def show
     @cart_items = CartItem.where(user_id: current_user.id)
     if @cart_items.empty?
       redirect_to root_path
     end
   end
+
+end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :item
     belongs_to :user
+
+    validates :title, presence: true, length: { minimum: 1 }
+    validates :body, presence: true, length: { minimum: 5 }
 end
