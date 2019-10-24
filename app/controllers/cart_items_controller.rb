72ca8e9354b5eed2e2b29074602eb561296eefb2
高橋智也 <takahashitomoya@takahashitomoyanoMacBook-Pro.local>
2019-10-24 16:13:49 +0900

    検索機能、cart_itemsのcontroller修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,14 +1,21 @@
 class CartItemsController < ApplicationController
   def create
-    @cart_item = CartItem.new
-    @cart_item.user_id = current_user.id
-    @cart_item.amount = params[:cart_item][:amount].to_i
-    @cart_item.item_id = params[:cart_item][:item_id].to_i
-    if @cart_item.save
-    redirect_to cart_items_path
+    if CartItem.find_by(item_id: params[:cart_item][:item_id].to_i)
+      @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id].to_i)
+      amount = params[:cart_item][:amount].to_i + CartItem.find_by(item_id: params[:cart_item][:item_id]).amount
+      @cart_item.update(amount: amount)
+      redirect_to cart_items_path
     else
-      @items = Item.page(params[:page]).per(10).reverse_order
-      render 'items/index'
+      @cart_item = CartItem.new
+      @cart_item.user_id = current_user.id
+      @cart_item.amount = params[:cart_item][:amount].to_i
+      @cart_item.item_id = params[:cart_item][:item_id].to_i
+        if @cart_item.save
+          redirect_to cart_items_path
+        else
+          @items = Item.page(params[:page]).per(10).reverse_order
+          render 'items/index'
+        end
     end
   end
 
@@ -23,5 +30,6 @@ class CartItemsController < ApplicationController
     if @cart_items.empty?
       redirect_to root_path
     end
+    
   end
 end
