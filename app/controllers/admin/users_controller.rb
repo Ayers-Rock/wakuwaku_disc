3d25be032cc8c2191cf48a7f712bc7de238207d1
trudemui <alesth_130@yahoo.co.jp>
2019-10-24 18:39:05 +0900

    fix

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -23,7 +23,7 @@ class Admin::UsersController < Admin::AdminApplicationsController
 
   def update
     @user = User.find(params[:id])
-    # binding.pry
+    binding.pry
     if @user.update(user_params)
       # binding.pry
       redirect_to admin_user_path(@user.id)
