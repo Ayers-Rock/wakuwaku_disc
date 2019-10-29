37842f2707641aec1b9f48fe0b661fc038397dfd
Spanaic <spanaic@gmail.com>
2019-10-29 16:34:53 +0900

    pull前

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
     passwords:     'admins/passwords',
     registrations: 'admins/registrations'
   }
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   namespace :admin do
 
     get '' => 'admins#top'
